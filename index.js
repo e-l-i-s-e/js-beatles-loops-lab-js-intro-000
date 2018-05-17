eec8d3801526b4fb2503b2f80027a7de2a197a39
Elise Bonner <elise0209@mac.com>
2018-05-17 18:40:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ function theBeatlesPlay(arrayMusicians, arrayInstruments){
     newArray.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
   } return newArray;
 }
+
+function johnLennonFacts(array){
+  var i = 0
+  while (i < array.length){
+    array[i] = array[i] + "!!!";
+    i++;
+  } return array;
+}
+
+function iLoveTheBeatles(num){
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    num++;
+  } while (num < 15)
+  return array;
+}
